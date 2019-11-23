741e89e04904240d8737f9540660e5239660679c
Austin de Coup-Crank <austindcc@gmail.com>
2019-11-22 17:00:39 -0800

    Black

--- concurrency/exercise2.py
+++ concurrency/exercise2.py
@@ -7,29 +7,19 @@ USERNAME = os.environ.get("NET_USER") or input("Username: ")
 PASSWORD = os.environ.get("NET_PASS") or getpass()
 
 devices = [
-    {
-        "host": "cisco3.lasthop.io",
-        "device_type": "cisco_ios",
-    },
-    {
-        "host": "cisco4.lasthop.io",
-        "device_type": "cisco_ios",
-    },
-    {
-        "host": "arista1.lasthop.io",
-        "device_type": "arista_eos",
-    },
-    {
-        "host": "arista2.lasthop.io",
-        "device_type": "arista_eos",
-    },
+    {"host": "cisco3.lasthop.io", "device_type": "cisco_ios"},
+    {"host": "cisco4.lasthop.io", "device_type": "cisco_ios"},
+    {"host": "arista1.lasthop.io", "device_type": "arista_eos"},
+    {"host": "arista2.lasthop.io", "device_type": "arista_eos"},
 ]
+
 
 def show_command(dev, cmd):
     conn = ConnectHandler(**dev)
     output = conn.send_command(cmd)
     conn.disconnect()
     return output
+
 
 def main():
     pool = ThreadPoolExecutor(max_workers=8)
@@ -38,14 +28,12 @@ def main():
         dev["username"] = USERNAME
         dev["password"] = PASSWORD
         threads.append(pool.submit(show_command, dev, "show ip arp"))
-    
+
     for thread in as_completed(threads):
         print()
         print(thread.result())
         print()
 
+
 if __name__ == "__main__":
     main()
-
-
-
